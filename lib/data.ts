5525c709cd1ca18277a74b97097f0196d91049d1
Andreselectroman <59124453+Andreselectroman@users.noreply.github.com>
2024-10-14 13:04:59 -0500

    modificacion1
    primera modificacion lib

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,7 @@
 import banner1 from "@/public/assets/banner/banner1.jpg";
 import banner2 from "@/public/assets/banner/banner2.jpg";
 import banner3 from "@/public/assets/banner/banner3.jpg";
+import { text } from "stream/consumers";
 
 
 
@@ -8,22 +9,35 @@ import banner3 from "@/public/assets/banner/banner3.jpg";
 export const bannerItems = [
   {
     id: 1,
-    content: "Ea minim reprehenderit eiusmod eu mollit.",
-    linkText: "Nisi exercitation proident culpa ad duis.",
+    content: "Utilizamos la tecnologia centrada en el Evangelio.",
+   
+    texts: [
+      "//Porque la Tierra sera llena del conocimiento",
+      "de la gloria de Jehova como las aguas cubren",
+      "la mar.",
+    ],
     image: banner1,
     linkHref: "/contacto",
   },
   {
     id: 2,
-    content: "Ipsum non consequat minim ea officia est tempor.",
-    linkText: "Voluptate cupidatat dolore anim nisi officia deserunt nostrud culpa enim irure sunt.",
+    content: "Utilizamos la tecnologia centrada en el Evangelio.",
+    
+    texts: [
+      "Hasta 50% de descuento en productos seleccionados.",
+      "No te pierdas nuestras ventas relámpago cada viernes.",
+    ],
     image: banner2,
     linkHref: "/contacto2",
   },
   {
     id: 3,
-    content: "Cillum velit tempor tempor tempor ipsum occaecat.",
-    linkText: "Irure excepteur velit culpa aute ut ea dolore duis.",
+    content: "Utilizamos la tecnologia centrada en el Evangelio.",
+   
+    texts: [
+      "Hasta 50% de descuento en productos seleccionados.",
+      "No te pierdas nuestras ventas relámpago cada viernes.",
+    ],
     image: banner3,
     linkHref: "/contacto3",
   },
